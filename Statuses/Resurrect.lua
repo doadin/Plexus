55ecdf72c9b03001353b07ba3f30de7eb4e3ce7a
tbkizle <tbkizle@gmail.com>
2020-05-06 16:16:02 -0400

    [Statuses\Ressurrect] Minor Cleanup

--- Statuses/Resurrect.lua
+++ Statuses/Resurrect.lua
@@ -13,6 +13,7 @@ local _, Plexus = ...
 local L = Plexus.L
 
 local LibResInfo
+local UnitCastingInfo = _G.UnitCastingInfo
 local PlexusRoster = Plexus:GetModule("PlexusRoster")
 
 local PlexusStatusResurrect = Plexus:NewStatusModule("PlexusStatusResurrect", "AceTimer-3.0")
@@ -38,10 +39,10 @@ local extraOptionsForStatus = {
         desc = L["Show the status until the resurrection is accepted or expires, instead of only while it is being cast."],
         type = "toggle",
         width = "double",
-        get = function(t)
+        get = function(t) --luacheck: ignore 212
             return PlexusStatusResurrect.db.profile.alert_resurrect.showUntilUsed
         end,
-        set = function(t, v)
+        set = function(t, v) --luacheck: ignore 212
             PlexusStatusResurrect.db.profile.alert_resurrect.showUntilUsed = v
             PlexusStatusResurrect:UpdateAllUnits()
         end,
@@ -58,11 +59,11 @@ local extraOptionsForStatus = {
                 desc = L["Use this color for resurrections that are currently being cast."],
                 type = "color",
                 hasAlpha = true,
-                get = function(t)
+                get = function(t) --luacheck: ignore 212
                     local color = PlexusStatusResurrect.db.profile.alert_resurrect.color
                     return color.r, color.g, color.b, color.a or 1
                 end,
-                set = function(t, r, g, b, a)
+                set = function(t, r, g, b, a) --luacheck: ignore 212
                     local color = PlexusStatusResurrect.db.profile.alert_resurrect.color
                     color.r, color.g, color.b, color.a = r, g, b, a or 1
                 end,
@@ -73,11 +74,11 @@ local extraOptionsForStatus = {
                 desc = L["Use this color for resurrections that have finished casting and are waiting to be accepted."],
                 type = "color",
                 hasAlpha = true,
-                get = function(t)
+                get = function(t) --luacheck: ignore 212
                     local color = PlexusStatusResurrect.db.profile.alert_resurrect.color2
                     return color.r, color.g, color.b, color.a or 1
                 end,
-                set = function(t, r, g, b, a)
+                set = function(t, r, g, b, a) --luacheck: ignore 212
                     local color = PlexusStatusResurrect.db.profile.alert_resurrect.color2
                     color.r, color.g, color.b, color.a = r, g, b, a or 1
                 end,
@@ -88,11 +89,11 @@ local extraOptionsForStatus = {
                 desc = L["Use this color for pre-cast Soulstones that are waiting to be accepted."],
                 type = "color",
                 hasAlpha = true,
-                get = function(t)
+                get = function(t) --luacheck: ignore 212
                     local color = PlexusStatusResurrect.db.profile.alert_resurrect.color3
                     return color.r, color.g, color.b, color.a or 1
                 end,
-                set = function(t, r, g, b, a)
+                set = function(t, r, g, b, a) --luacheck: ignore 212
                     local color = PlexusStatusResurrect.db.profile.alert_resurrect.color3
                     color.r, color.g, color.b, color.a = r, g, b, a or 1
                 end,
@@ -137,7 +138,7 @@ function PlexusStatusResurrect:UpdateAllUnits(event)
     end
 end
 
-function PlexusStatusResurrect:HandleCallback(callback, targetUnit, targetGUID, casterUnit, casterGUID, endTime)
+function PlexusStatusResurrect:HandleCallback(callback, targetUnit, targetGUID, casterUnit)
     if strsub(callback, 1, 18) == "LibResInfo_MassRes" then
         self:Debug(callback, casterUnit)
         self:UpdateAllUnits()
@@ -149,11 +150,11 @@ end
 
 function PlexusStatusResurrect:UpdateUnit(unit, guid)
     if not unit then return end
-    if not guid then guid = UnitGUID(unitid) end
+    if not guid then guid = UnitGUID(unit) end
     if not PlexusRoster:IsGUIDInRaid(guid) then return end
 
     local db = self.db.profile.alert_resurrect
-    local hasRes, endTime, casterUnit, casterGUID = LibResInfo:UnitHasIncomingRes(guid)
+    local hasRes, endTime, casterUnit = LibResInfo:UnitHasIncomingRes(guid)
 
     if not hasRes or (hasRes == "PENDING" and not db.showUntilUsed) then
         return self.core:SendStatusLost(guid, "alert_resurrect")
