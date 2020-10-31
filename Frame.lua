ca87bfba5f709d67247a5beae3bc5b060666ee44
tbkizle <tbkizle@gmail.com>
2020-10-31 01:21:05 -0400

    [Frame] Fix Option to Disable Corner Icons

--- Frame.lua
+++ Frame.lua
@@ -1054,17 +1054,17 @@ function PlexusFrame:UpdateOptionsForIndicator(indicator, name, order)
     end
 
     if indicator == "tooltip" then
-        self:Debug("indicator text3 is disabled")
+        self:Debug("indicator tooltip is disabled")
         menu[indicator] = nil
         return
     end
 
-    if (string.find(indicator, "ei_corner") and (string.find(indicator, "2")) and not self.db.profile.enableCorner2) then --luacheck:ignore 631
+    if (string.find(name, "Indicator") and (string.find(name, "2")) and not self.db.profile.enableCorner2) then --luacheck:ignore 631
         self:Debug("Disabling Corners 2")
         menu[indicator] = nil
         return
     end
-    if (string.find(indicator, "ei_corner") and (string.find(indicator, "3") or string.find(indicator, "4")) and not self.db.profile.enableCorner34) then --luacheck:ignore 631
+    if (string.find(name, "Indicator") and (string.find(name, "3") or string.find(name, "4")) and not self.db.profile.enableCorner34) then --luacheck:ignore 631
         self:Debug("Disabling Corners 3-4")
         menu[indicator] = nil
         return
@@ -1122,10 +1122,10 @@ function PlexusFrame:UpdateOptionsForIndicator(indicator, name, order)
         if indicator == "tooltip" then
             menu[indicator].disabled = function() return true end
         end
-        if (string.find(indicator, "ei_corner") and string.find(indicator, "2")) then
+        if (string.find(name, "Indicator") and string.find(name, "2")) then
             menu[indicator].disabled = function() return not PlexusFrame.db.profile.enableCorner2 end
         end
-        if (string.find(indicator, "ei_corner") and (string.find(indicator, "3") or string.find(indicator, "4"))) then
+        if (string.find(name, "Indicator") and (string.find(name, "3") or string.find(name, "4"))) then
             menu[indicator].disabled = function() return not PlexusFrame.db.profile.enableCorner34 end
         end
         if (string.find(indicator, "ei_icon") and string.find(indicator, "2")) then
