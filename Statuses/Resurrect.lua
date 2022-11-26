1afeaf0a141c00811c1341ddecc8b7e938320f03
Elnarfim <37562642+Elnarfim@users.noreply.github.com>
2022-11-26 20:32:22 +0900

    Update ressurect for DF

--- Statuses/Resurrect.lua
+++ Statuses/Resurrect.lua
@@ -89,27 +89,16 @@ local ResSpells = {
     [115178] = GetSpellInfo(115178), -- Resuscitate (Monk)
     [50769]  = GetSpellInfo(50769),  -- Revive (Druid)
     [20484]  = GetSpellInfo(20484),  -- Rebirth (Druid)
-    --[982]    = GetSpellInfo(982),    -- Revive Pet (Hunter)
+    [361227] = GetSpellInfo(361227), -- Return (Evoker)
     -- Items
     [8342]   = GetSpellInfo(8342),   -- Defibrillate (Goblin Jumper Cables)
     [22999]  = GetSpellInfo(22999),  -- Defibrillate (Goblin Jumper Cables XL)
     [54732]  = GetSpellInfo(54732),  -- Defibrillate (Gnomish Army Knife)
     [164729] = GetSpellInfo(164729), -- Defibrillate (Ultimate Gnomish Army Knife)
     [265116] = GetSpellInfo(265116), -- Defibrillate (Unstable Temporal Time Shifter)
-    [199119] = GetSpellInfo(199119), -- Failure Detection Aura (Failure Detection Pylon) -- NEEDS CHECK
     [187777] = GetSpellInfo(187777), -- Reawaken (Brazier of Awakening)
-    -- massSpells
-    --[212056] = GetSpellInfo(212056), -- Absolution (Holy Paladin)
-    --[212048] = GetSpellInfo(212048), -- Ancestral Vision (Restoration Shaman)
-    --[212036] = GetSpellInfo(212036), -- Mass Resurrection (Discipline/Holy Priest)
-    --[212051] = GetSpellInfo(212051), -- Reawaken (Mistweaver Monk)
-    --[212040] = GetSpellInfo(212040), -- Revitalize (Restoration Druid)
     -- pening souslstone ank etc
     [160029] = GetSpellInfo(160029), -- Resurrecting aka pending
-    --[27740] = GetSpellInfo(27740), -- Reincarnation
-    --[20608] = GetSpellInfo(20608), -- Reincarnation
-    --[225080] = GetSpellInfo(225080), -- Reincarnation
-    --[21169] = GetSpellInfo(21169), -- Reincarnation
 }
 local MassResSpells = {
     -- massSpells
@@ -117,7 +106,8 @@ local MassResSpells = {
     [212048] = GetSpellInfo(212048), -- Ancestral Vision (Restoration Shaman)
     [212036] = GetSpellInfo(212036), -- Mass Resurrection (Discipline/Holy Priest)
     [212051] = GetSpellInfo(212051), -- Reawaken (Mistweaver Monk)
-    [212040] = GetSpellInfo(212040) -- Revitalize (Restoration Druid)
+    [212040] = GetSpellInfo(212040), -- Revitalize (Restoration Druid)
+    [361178] = GetSpellInfo(361178)  -- Mass Return (Preservation Evoker)
 }
 
 ------------------------------------------------------------------------
